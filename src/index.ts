a9b314f4d4d27b0b84754c0302f2da7be101b0f9
Naaman Curtis <ncurtis@calmid.com>
2021-09-10 12:22:56 +0100

    feat(api): update debounced fan out to work correctly

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,16 @@ export {
   INITIAL_ROOM_STATE,
   generateStateDiff,
 } from './roomSlice';
-export { RootState, ContextPayload, Context, store } from './store';
+export { RootState } from './store';
+
+export type ID = string;
+export interface Context {
+  userId: ID;
+  isTeacher: boolean;
+  name: string;
+}
+
+export type ContextPayload<T> = {
+  context: Context;
+  payload: T;
+};
--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { roomReducer } from './roomSlice';
 
-export type ID = string;
-export interface Context {
-  userId: ID;
-  isTeacher: boolean;
-  name: string;
-}
-
-export type ContextPayload<T> = {
-  context: Context;
-  payload: T;
-};
-
-export const store = configureStore({
+const store = configureStore({
   middleware: [],
   reducer: {
     room: roomReducer,
