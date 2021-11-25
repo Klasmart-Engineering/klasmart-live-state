075da8e3a225cbc51a817238f847cd67eb28c44b
Owen Delahoy <owen.delahoy@gmail.com>
2021-11-25 18:10:57 +0900

    add equalityFn to useNetworkState

--- src/react/hooks/state.ts
+++ src/react/hooks/state.ts
@@ -3,7 +3,10 @@ import NetworkContext from '../context';
 import { useSelector } from 'react-redux';
 import { State } from '../../redux/reducer';
 
-export function useNetworkState<T=unknown>(selector: (state: State) => T) {
+export function useNetworkState<T=unknown>(
+  selector: (state: State) => T,
+  equalityCheck?: (left: T, right: T) => boolean,
+) {
   const network = useContext(NetworkContext);
-  return useSelector((state) => selector(network.selector(state)))
+  return useSelector(state => selector(network.selector(state)), equalityCheck)
 }
