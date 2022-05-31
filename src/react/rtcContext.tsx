5e30f7eb4bd64d81a89e85f22e89a13a50dc265b
Evgeny Roskach <98022544+evgeny-roskach-kidsloop@users.noreply.github.com>
2022-05-31 12:16:19 +0900

    feat(sfu-146): use track states hook (#17)
    * feat(sfu-146): keep track of tracks' states in the SFU class + react hook
    
    refactor: restructure useEffect to handle track state
    
    fix: use Promise.all instead of Pormise.allSettled
    
    feat: keep track of tracks' states in the SFU class
    
    refactor: style fixes
    
    feat: emit update tracks event when deleting track + remove dead code
    
    refactor: style fixes
    Signed-off-by: genyrosk <e.roskach@gmail.com>
    Signed-off-by: evgeny-roskach-kidsloop <evgeny.roskach@kidsloop.live>
    
    * chore: update package-lock + remove console.log

--- src/react/rtcContext.tsx
+++ src/react/rtcContext.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TrackSender } from "../network/trackSender";
-import {SFU, SfuId, SfuAuthErrors, SfuConnectionError} from "../network/sfu";
+import {SFU, SfuId, SfuAuthErrors, SfuConnectionError, DerivedTrackState} from "../network/sfu";
 import {Room, TrackLocation} from "../network/room";
 
 
@@ -177,6 +177,19 @@ export class WebRtcManager {
     }
 
     private readonly sfus = new Map<SfuId, SFU>();
+    public get sfuIdsHash() {
+        return Array.from(this.sfus.keys()).sort().join(",");
+    }
+    public subscribeToSfuTrackStateUpdates(callback: (state: DerivedTrackState[]) => void) {
+        for (const sfu of this.sfus.values()) {
+            sfu.emitter.on("trackStatesUpdate", callback);
+        }
+    }
+    public unsubscribeToSfuTrackStateUpdates(callback: (state: DerivedTrackState[]) => void) {
+        for (const sfu of this.sfus.values()) {
+            sfu.emitter.off("trackStatesUpdate", callback);
+        }
+    }
 
     public sfu(id: SfuId) {
         let sfu = this.sfus.get(id);
@@ -199,6 +212,12 @@ export class WebRtcManager {
         const wsEndpoint = new URL(this.baseEndpoint.toString());
         wsEndpoint.pathname += `sfuid/${id}`;
         return wsEndpoint.toString();
+    }
+
+    public getAllSfuTrackStates() {
+        return Array.from(this.sfus.values()).flatMap((sfu) => {
+            return Array.from(sfu.trackStates.values());
+        });
     }
 }
 
